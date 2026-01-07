94a8369d7a889cfa12f742819667d036f8703be0
mellurboo <mellurblur@gmail.com>
2026-01-07 19:40:00 +0000

    ACPI Tables Repaired

--- kernel/src/ACPI/acpi_priv.h
+++ kernel/src/ACPI/acpi_priv.h
@@ -44,43 +44,73 @@ struct acpi_xsdt {
     uint64_t tables[];
 } __attribute__((packed));
 
-struct acpi_fadt {
-    struct acpi_sdt header;
+typedef struct
+{
+    uint8_t address_space_id;
+    uint8_t register_bit_width;
+    uint8_t register_bit_offset;
+    uint8_t access_size;
+    uint64_t address;
+} __attribute__((packed)) GenericAddressStructure;
 
+typedef struct acpi_fadt
+{
+    struct acpi_sdt header;
     uint32_t firmware_ctrl;
     uint32_t dsdt;
-
-    uint8_t  reserved;
-
-    uint8_t  preferred_pm_profile;
+    uint8_t __reserved;
+    uint8_t preferred_pm_profile;
     uint16_t sci_int;
     uint32_t smi_cmd;
-    uint8_t  acpi_enable;
-    uint8_t  acpi_disable;
-
-    uint8_t  S4BIOS_req;
-    uint8_t  pstate_cnt;
-
+    uint8_t acpi_enable;
+    uint8_t acpi_disable;
+    uint8_t s4bios_req;
+    uint8_t pstate_cnt;
     uint32_t pm1a_evt_blk;
     uint32_t pm1b_evt_blk;
     uint32_t pm1a_cnt_blk;
     uint32_t pm1b_cnt_blk;
-
-    // acpi2
-    uint64_t reset_reg_address;
-    uint8_t  reset_value;
-    uint8_t  reserved2[3];
-
-    // more later acpi is huge
-
-} __attribute__((packed));
-
-struct acpi_gas {
-    uint8_t  space_id;    // 0=RAM 1=I/O
-    uint8_t  bit_width;
-    uint8_t  bit_offset;
-    uint8_t  access_size;
-    uint64_t address;
-} __attribute__((packed));
+    uint32_t pm2_cnt_blk;
+    uint32_t pm_tmr_blk;
+    uint32_t gpe0_blk;
+    uint32_t gpe1_blk;
+    uint8_t pm1_evt_len;
+    uint8_t pm1_cnt_len;
+    uint8_t pm2_cnt_len;
+    uint8_t pm_tmr_len;
+    uint8_t gpe0_blk_len;
+    uint8_t gpe1_blk_len;
+    uint8_t gpe1_base;
+    uint8_t cst_cnt;
+    uint16_t p_lvl2_lat;
+    uint16_t p_lvl3_lat;
+    uint16_t flush_size;
+    uint16_t flush_stride;
+    uint8_t duty_offset;
+    uint8_t duty_width;
+    uint8_t day_alrm;
+    uint8_t mon_alrm;
+    uint8_t century;
+    uint16_t iapc_boot_arch;
+    uint8_t __reserved2;
+    uint32_t flags;
+    GenericAddressStructure reset_reg;
+    uint8_t reset_value;
+    uint16_t arm_boot_arch;
+    uint8_t fadt_minor_version;
+    uint64_t x_firmware_ctrl;
+    uint64_t x_dsdt;
+    uint8_t x_pm1a_evt_blk[12];
+    uint8_t x_pm1b_evt_blk[12];
+    uint8_t x_pm1a_cnt_blk[12];
+    uint8_t x_pm1b_cnt_blk[12];
+    uint8_t x_pm2_cnt_blk[12];
+    uint8_t x_pm_tmr_blk[12];
+    uint8_t x_gpe0_blk[12];
+    uint8_t x_gpe1_blk[12];
+    uint8_t sleep_control_reg[12];
+    uint8_t sleep_status_reg[12];
+    uint64_t hypervison_vendor_identity;
+} __attribute__((packed)) fadt_t;
 
 extern struct acpi_fadt *fadt;
--- kernel/src/ACPI/impl/acpi_power.c
+++ kernel/src/ACPI/impl/acpi_power.c
@@ -6,12 +6,13 @@
 
 __attribute__((noreturn))
 void acpi_shutdown(void) {
-    uint16_t cmd = ACPI_PM1_SLEEP_CMD(ACPI_S5);
+    uint16_t cmd = ACPI_PM1_SLEEP_CMD(0x5);
     outw(PM1A_CNT, cmd);
     if (PM1B_CNT)
         outw(PM1B_CNT, cmd);
-    for (;;)
-        asm volatile("hlt");
+
+    asm volatile("cli");
+    for(;;){}
 }
 
 __attribute__((noreturn))
@@ -19,14 +20,14 @@ void acpi_reboot(void) {
     if (!fadt)
         ke_panic("ACPI FADT not present");
 
-    if (fadt->reset_reg_address == 0)
+    if (fadt->reset_reg.address == 0)
         ke_panic("ACPI reset register not present");
 
-    if (fadt->reset_reg_address <= 0xFFFF) {
-        outb((uint16_t)fadt->reset_reg_address, fadt->reset_value);
+    if (fadt->reset_reg.address <= 0xFFFF) {
+        outb((uint16_t)fadt->reset_reg.address, fadt->reset_value);
     } else {
-        paddr_t phys_page = fadt->reset_reg_address & ~(PAGE_SIZE_4K - 1);
-        size_t offset = fadt->reset_reg_address & (PAGE_SIZE_4K - 1);
+        paddr_t phys_page = fadt->reset_reg.address & ~(PAGE_SIZE_4K - 1);
+        size_t offset = fadt->reset_reg.address & (PAGE_SIZE_4K - 1);
 
         void *vpage = NULL;
         paging_alloc_empty_frame(&vpage);
@@ -40,7 +41,7 @@ void acpi_reboot(void) {
     }
 
     kprintf("ACPI Reboot attempted: Reset Register Address: %p, Reset Value: %u\n", 
-            (void *)fadt->reset_reg_address, fadt->reset_value);
+            (void *)fadt->reset_reg.address, fadt->reset_value);
     for (;;)
         __asm__("hlt");
 }
