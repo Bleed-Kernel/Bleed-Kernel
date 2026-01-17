979656c23a439ed2ba26e3b0960cb915fde113eb
mellurboo <mellurblur@gmail.com>
2026-01-17 03:20:46 +0000

    fixed a sysread issue

--- kernel/src/syscalls/impl/sys_read.c
+++ kernel/src/syscalls/impl/sys_read.c
@@ -5,9 +5,12 @@ uint64_t sys_read(uint64_t fd, uint64_t user_buf, uint64_t len) {
     if (fd >= MAX_FDS || !current_fd_table) return -1;
 
     file_t* f = current_fd_table->fds[fd];
-    if (!f || !(f->flags & O_RDONLY || f->flags & O_RDWR)) return -1;
+    if (!f || ((f->flags & O_RDONLY) == 0 && (f->flags & O_RDWR) == 0)) return -1;
 
     long r = inode_read(f->inode, (void*)user_buf, len, f->offset);
     if (r > 0) f->offset += r;
+
+    if (r < 0 && f->offset >= vfs_filesize(f->inode)) return 0;
+
     return r;
 }
