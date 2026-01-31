72e93a2dd2561cd0297cdb33399c867b34e7d2c5
mellurboo <mellurblur@gmail.com>
2026-01-31 02:49:07 +0000

    fixed perms on sys_write

--- kernel/src/syscalls/impl/sys_write.c
+++ kernel/src/syscalls/impl/sys_write.c
@@ -4,17 +4,25 @@
 #include <string.h>
 
 uint64_t sys_write(uint64_t fd, uint64_t user_buf, uint64_t len) {
-    if (fd >= MAX_FDS || !current_fd_table || !user_buf || len == 0) return 0;
+    if (fd >= MAX_FDS || !current_fd_table || !user_buf || len == 0)
+        return -1;
 
     file_t* f = current_fd_table->fds[fd];
-    if (!f || !(f->flags & O_WRONLY || f->flags & O_RDWR)) return -1;
+    if (!f)
+        return -1;
+
+    int mode = f->flags & O_MODE;
+    if (mode != O_WRONLY && mode != O_RDWR)
+        return -1;
 
     char* kbuf = kmalloc(len);
-    if (!kbuf) return -1;
+    if (!kbuf)
+        return -1;
 
     memcpy(kbuf, (void*)user_buf, len);
     long written = inode_write(f->inode, kbuf, len, f->offset);
-    if (written > 0) f->offset += written;
+    if (written > 0)
+        f->offset += written;
 
     kfree(kbuf, len);
     return written;
