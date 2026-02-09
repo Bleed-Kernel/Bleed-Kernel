2aebf42258730bbc5d1573ef0e1b2d1c6abee653
mellurboo <mellurblur@gmail.com>
2026-02-09 09:17:41 +0000

    Revamped Panic, including crash signature

--- kernel/src/cpu/exception/exception_handler.c
+++ kernel/src/cpu/exception/exception_handler.c
@@ -1,11 +1,3 @@
-/*
-    This file has been dramatically changed:
-        Before, there was an issue that made kernel panics
-        useless, becuase RIP would always point to the 
-        save cpu registers function, this reform
-        aims to completly fix that silly issue
-*/
-
 #include <stdint.h>
 #include <stdio.h>
 #include <ansii.h>
@@ -14,22 +6,13 @@
 #include <drivers/framebuffer/framebuffer.h>
 #include <sched/scheduler.h>
 
+static int faulted = 0;
+
 struct isr_stackframe {
-    uint64_t rax;
-    uint64_t rbx;
-    uint64_t rcx;
-    uint64_t rdx;
-    uint64_t rsi;
-    uint64_t rdi;
-    uint64_t rbp;
-    uint64_t r8;
-    uint64_t r9;
-    uint64_t r10;
-    uint64_t r11;
-    uint64_t r12;
-    uint64_t r13;
-    uint64_t r14;
-    uint64_t r15;
+    uint64_t rax; uint64_t rbx; uint64_t rcx; uint64_t rdx;
+    uint64_t rsi; uint64_t rdi; uint64_t rbp;
+    uint64_t r8;  uint64_t r9;  uint64_t r10; uint64_t r11;
+    uint64_t r12; uint64_t r13; uint64_t r14; uint64_t r15;
     uint64_t vector;
     uint64_t error_code;
     uint64_t rip;
@@ -41,91 +24,141 @@ const char* exception_name(uint8_t vector) {
     static const char* names[32] = {
         "Divide Error", "Debug", "Non-maskable Interrupt", "Breakpoint",
         "Overflow", "Bound Range Exceeded", "Invalid Opcode", "Device Not Available",
-        "Double Fault", "Coprocessor Segment Overrun", "Invalid tss_t", "Segment Not Present",
+        "Double Fault", "Coprocessor Segment Overrun", "Invalid TSS", "Segment Not Present",
         "Stack Segment Fault", "General Protection Fault", "Page Fault", "Reserved",
-        "x87 FPU Floating-Point Error", "Alignment Check", "Machine Check", "SIMD Floating-Point Exception",
-        "Virtualization Exception", "Control Protection Exception", "Reserved", "Reserved",
-        "Reserved", "Reserved", "Reserved", "Reserved", "Reserved", "Reserved", "Security Exception", "Reserved"
+        "x87 FPU Error", "Alignment Check", "Machine Check", "SIMD Exception",
+        "Virtualization Exception", "Control Protection", "Reserved", "Reserved",
+        "Reserved", "Reserved", "Reserved", "Reserved", "Reserved", "Reserved", 
+        "Security Exception", "Reserved"
     };
-    return vector < 32 ? names[vector] : "Unknown";
+    return vector < 32 ? names[vector] : "Unknown Internal Exception";
 }
 
-extern void* ke_exception_handler(void *frame){
+static void dump_page_fault_info(uint64_t err, uint64_t cr2) {
+    kprintf("\n  " RGB_FG(255, 100, 100) "PAGE FAULT" RESET "\n");
+    kprintf(GRAY_FG "  Address   : " CYAN_FG "0x%p\n" RESET, (void*)cr2);
+
+    const char* access = (err & (1 << 4)) ? "EXECUTE (Instruction Fetch)" : 
+                         (err & (1 << 1)) ? "WRITE" : "READ";
+
+    const char* privilege = (err & (1 << 2)) ? "USER" : "SUPERVISOR";
+
+    const char* violation;
+    if (!(err & (1 << 0))) {
+        violation = "PAGE NOT PRESENT";
+    } else if (err & (1 << 3)) {
+        violation = "RESERVED BIT VIOLATION";
+    } else if (err & (1 << 5)) {
+        violation = "PROTECTION KEY VIOLATION";
+    } else if (err & (1 << 15)) {
+        violation = "SGX VIOLATION";
+    } else {
+        violation = "ACCESS RIGHTS VIOLATION";
+    }
+
+    kprintf(GRAY_FG "  Action    : " WHITE_FG "%s\n" RESET, access);
+    kprintf(GRAY_FG "  Privilege : " WHITE_FG "%s\n" RESET, privilege);
+    kprintf(GRAY_FG "  Page Flags: " RED_FG "%s\n" RESET, violation);
+
+    if (!(err & (1 << 0)) && cr2 < 0x1000) {
+        kprintf(ORANGE_FG "            : NULL pointer dereference\n" RESET);
+    } else if ((err & (1 << 4)) && (err & (1 << 0))) {
+        kprintf(ORANGE_FG "            : NX Violation\n" RESET);
+    }
+}
+
+static inline void print_separator(const char* title) {
+    kprintf(GRAY_FG "\n--[ " WHITE_FG "%s " GRAY_FG "]" 
+            "----------------------------------------------------\n" RESET, title);
+}
+
+struct __attribute__((packed)) panic_signature {
+    uint64_t rip;
+    uint64_t cr2;
+    uint32_t error_code;
+    uint8_t  vector;
+};
+
+static void generate_signature(void* data, uint64_t len) {
+    const char* table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    uint8_t* bytes = (uint8_t*)data;
+
+    for (uint64_t i = 0; i < len; i += 3) {
+        if (i > 0 && (i % 6 == 0)) {
+            kprintf(GRAY_FG " " YELLOW_FG);
+        }
+
+        uint32_t b = (bytes[i] << 16);
+        if (i + 1 < len) b |= (bytes[i + 1] << 8);
+        if (i + 2 < len) b |= bytes[i + 2];
+
+        kprintf("%c%c", table[(b >> 18) & 0x3F], table[(b >> 12) & 0x3F]);
+        if (i + 1 < len) kprintf("%c", table[(b >> 6) & 0x3F]);
+        if (i + 2 < len) kprintf("%c", table[b & 0x3F]);
+    }
+    kprintf("\n");
+}
+
+extern void* ke_exception_handler(void *frame) {
+    if (faulted){
+        serial_write("A fault happend in the panic handler, its probably propogated through whatever caused it in the first place but we wont spam the screen\n");
+        for(;;) { __asm__ volatile ("cli; hlt"); }
+    }
+    faulted = 1;
+    kprintf("fault!"); // at the moment clearing the screen while the screen is empty causes ANOTHER panic
+    kprintf("\x1b[J");
     __asm__ volatile ("cli");
     struct isr_stackframe *f = (struct isr_stackframe *)frame;
-    
-    /* its really buggy and only works sometimes so ill remove it for now
-    if ((f->cs & 0x3) != 0) {
-        sched_mark_task_dead(get_current_task());
-        return sched_switch_context(f);
-    }
-    */
 
-    uint64_t vector = f->vector;
-    uint64_t err    = f->error_code;
-    uint64_t rip    = f->rip;
-    uint64_t cs     = f->cs;
-    uint64_t rflags = f->rflags;
+    serial_write("\n[PANIC] Vec:"); serial_write_hex(f->vector);
+    serial_write(" RIP:"); serial_write_hex(f->rip);
+
+    kprintf("\n  " RGB_FG(207, 45, 45) "KERNEL PANIC: ");
+    kprintf(RGB_FG(255, 80, 80) "CPU EXCEPTION: %s (0x%x)" RESET "\n", 
+            exception_name(f->vector), (uint8_t)f->vector);
+
+    print_separator("PROCESS CONTEXT");
+    kprintf("  Current Task: " GREEN_FG "%s" RESET " (PID: %llu)\n", get_current_task()->name, get_current_task()->id);
 
     uint64_t sym_addr;
-    const char *name = stack_trace_symbol_lookup(rip, &sym_addr);
+    const char *name = stack_trace_symbol_lookup(f->rip, &sym_addr);
+    
+    kprintf("  Instruction : " GREEN_FG "0x%p" RESET, (void*)f->rip);
+    if (name) kprintf(ORANGE_FG " <%s + 0x%llx>" RESET, name, f->rip - sym_addr);
+    kprintf("\n  Flags       : " GRAY_FG "0x%016llx" RESET " | CS: " GRAY_FG "0x%02x" RESET "\n", 
+            f->rflags, (uint16_t)f->cs);
 
-    serial_write("Kernel Panic!\n");
-    serial_write_hex(vector);
-    serial_write("\n");
-    serial_write_hex(f->rip);
-    serial_write("\n");
+    print_separator("CPU CONTEXT");
+    kprintf(GRAY_FG "  RAX " WHITE_FG "0x%016llx " GRAY_FG " R8  " WHITE_FG "0x%016llx " GRAY_FG " R12 " WHITE_FG "0x%016llx\n", f->rax, f->r8,  f->r12);
+    kprintf(GRAY_FG "  RBX " WHITE_FG "0x%016llx " GRAY_FG " R9  " WHITE_FG "0x%016llx " GRAY_FG " R13 " WHITE_FG "0x%016llx\n", f->rbx, f->r9,  f->r13);
+    kprintf(GRAY_FG "  RCX " WHITE_FG "0x%016llx " GRAY_FG " R10 " WHITE_FG "0x%016llx " GRAY_FG " R14 " WHITE_FG "0x%016llx\n", f->rcx, f->r10, f->r14);
+    kprintf(GRAY_FG "  RDX " WHITE_FG "0x%016llx " GRAY_FG " R11 " WHITE_FG "0x%016llx " GRAY_FG " R15 " WHITE_FG "0x%016llx\n", f->rdx, f->r11, f->r15);
+    kprintf(GRAY_FG "  RSI " WHITE_FG "0x%016llx " GRAY_FG " RDI " WHITE_FG "0x%016llx " GRAY_FG " RBP " WHITE_FG "0x%016llx\n", f->rsi, f->rdi, f->rbp);
+    
+    uint64_t cr2;
+    __asm__ volatile ("mov %%cr2, %0" : "=r"(cr2));
 
-    kprintf("\n");
-    kprintf(RED_FG "===============================================\n");
-    kprintf("   !!!  FATAL KERNEL EXCEPTION OCCURRED  !!!\n");
-    kprintf("===============================================\n" RESET);
-
-    kprintf(ORANGE_FG " (CPU Raised) EXCEPTION: %s\n" RESET, exception_name(vector));
-    kprintf(GREEN_FG " VECTOR: %llu   ERROR: 0x%p\n", vector, (void *)err);
-
-    kprintf("\n" ORANGE_FG " CPU STATE:" RESET "\n");   // todo: i didnt know you could litterally do this to use colour codes, ill change this across the rest of this file later
-    if (name) {
-        kprintf(" RIP: " GREEN_FG "0x%p" RESET "   CS: 0x%p   RFLAGS: 0x%p\n",
-                (void*)rip, (void*)cs, (void*)rflags);
-        kprintf("      " ORANGE_FG "<%s+0x%llu>%s\n", name, rip - sym_addr, RESET);
-    } else {
-        kprintf(" RIP: " GREEN_FG "0x%p" RESET "   CS: 0x%p   RFLAGS: 0x%p\n",
-                (void*)rip, (void*)cs, (void*)rflags);
+    if (f->vector == 14) {
+        dump_page_fault_info(f->error_code, cr2);
     }
 
-
-    kprintf("\n" ORANGE_FG " Registers:" RESET "\n");
-    kprintf(" RAX: %s0x%p%s\t  RBX: %s0x%p%s\t  RCX: %s0x%p%s\t  RDX: %s0x%p%s\t\n",
-            GREEN_FG, (void *)f->rax, RESET, GREEN_FG, (void *)f->rbx, RESET, GREEN_FG, (void *)f->rcx, RESET, GREEN_FG, (void *)f->rdx, RESET);
-
-    kprintf(" RSI: %s0x%p%s\t  RDI: %s0x%p%s\t  RBP: %s0x%p%s\t\n",
-            GREEN_FG, (void *)f->rsi, RESET, GREEN_FG, (void *)f->rdi, RESET, GREEN_FG, (void *)f->rbp, RESET);
-
-    kprintf(" R8 : %s0x%p%s\t  R9 : %s0x%p%s\t  R10: %s0x%p%s\t  R11: %s0x%p%s\t\n",
-            GREEN_FG, (void *)f->r8, RESET, GREEN_FG, (void *)f->r9, RESET, GREEN_FG, (void *)f->r10, RESET, GREEN_FG, (void *)f->r11, RESET);
-
-    kprintf(" R12: %s0x%p%s\t  R13: %s0x%p%s\t  R14: %s0x%p%s\t  R15: %s0x%p%s\t\n",
-            GREEN_FG, (void *)f->r12, RESET, GREEN_FG, (void *)f->r13, RESET, GREEN_FG, (void *)f->r14, RESET, GREEN_FG, (void *)f->r15, RESET);
-
-    if (vector == 14){
-        kprintf("\n" ORANGE_FG " PAGE FAULT DETAILS\n" RESET);
-        uint64_t cr2 = 0; 
-        __asm__ volatile ("mov %%cr2, %0" : "=r"(cr2));
-
-        kprintf("  Faulting address: " GREEN_FG "0x%p" RESET "\n", (void*)cr2);
-        kprintf("  Error code: %s 0x%p%s\n",GREEN_FG, (void*)err, RESET);
-        kprintf("   P  = %s%u%s\n", GREEN_FG, (unsigned)((err >> 0) & 1), RESET);
-        kprintf("   W/R= %s%u%s\n", GREEN_FG, (unsigned)((err >> 1) & 1), RESET);
-        kprintf("   U/S= %s%u%s\n", GREEN_FG, (unsigned)((err >> 2) & 1), RESET);
-        kprintf("   RSV= %s%u%s\n", GREEN_FG, (unsigned)((err >> 3) & 1), RESET);
-        kprintf("   I/D= %s%u%s\n", GREEN_FG, (unsigned)((err >> 4) & 1), RESET);
-    }
-
+    print_separator("BACKTRACE");
     uint64_t cur_rbp;
     __asm__ volatile ("mov %%rbp, %0" : "=r"(cur_rbp));
     stack_trace_print((uint64_t*)cur_rbp);
-    kprintf("\n" LOG_INFO " SYSTEM HALTED\n" RESET);
-    __asm__ volatile ("hlt");
-    for(;;){}
+
+    print_separator("DEBUG SIGNATURE");
+    kprintf(GRAY_FG "  Put this code into https://bleedkernel.com/panic.html or share it to report the issue\n\n" RESET);
+    kprintf("  " YELLOW_FG);
+
+    struct panic_signature signature = {
+        .rip = f->rip,
+        .cr2 = cr2,
+        .error_code = (uint32_t)f->error_code,
+        .vector = (uint8_t)f->vector
+    };
+
+    generate_signature(&signature, sizeof(signature));
+    
+    for(;;) { __asm__ volatile ("hlt"); }
 }
