08bd4ab94f61a13c37e28e632b4c20a35ffaa126
mellurboo <mellurblur@gmail.com>
2026-03-08 00:08:51 +0000

    Snapshot Build

--- kernel/include/kernel/kmain.h
+++ kernel/include/kernel/kmain.h
@@ -7,5 +7,4 @@ void kernel_self_test();
 tty_t kernel_console_init();
 int kernel_spawn_shell_on_tty(uint32_t tty_index);
 void kernel_request_shell_spawn(uint32_t tty_index);
-int kernel_has_shell_spawn_request(void);
 void kernel_service_shell_spawn_requests(void);
